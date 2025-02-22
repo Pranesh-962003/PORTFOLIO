0d0a37b3d913be7ac51b2e9ebd713c33a2f6f24d
Pranesh-962003 <pranesh962003.p@gmail.com>
2025-02-22 13:04:09 +0530

    first commit

---
+++ PortFolio/src/assets/assets.js
@@ -0,0 +1,87 @@
+import html from './html-124-svgrepo-com.svg'
+import css from './css.svg'
+import tail from './tailwind.svg'
+import js from './javascript.svg'
+import react from './reactjs.svg'
+import node from './node.svg'
+import express from './express.svg'
+import mongo from './mongo.svg'
+import sql from './mysql.svg'
+// import image from './WhatsApp Image 2024-12-16 at 13.07.17_1a2abf34.jpg'
+
+// export const logo_image = {
+//     image
+// }
+
+export const skills = [
+    {
+        image:html,
+        title:"HTML-5"
+    },
+    {
+        image:css,
+        title:'CSS-3'
+    },
+    {
+        image:tail,
+        title:"Tailwind CSS"
+    },
+    {
+        image:js,
+        title:"JavaScript",
+    },
+    {
+        image:react,
+        title:"React.js"
+    },
+    {
+        image:node,
+        title:"Node.js"
+    },
+    {
+        image:express,
+        title:"Express.js"
+    },
+    {
+        image:mongo,
+        title:"Mongodb"
+    },
+    {
+        image:sql,
+        title:"MySql"
+    },
+  
+]
+
+
+
+export const projects = [
+    {
+        title:"Doctolib",
+        des:"This project is a doctor appointment booking website with an integrated chatbot for healthcare assistance. It enables patients to book appointments, check doctor availability, and receive health-related guidance. The platform is built using React.js for the frontend, Node.js with Express.js for the backend, and MongoDB as the database. Hosted on Onrender.com, it integrates Chatbase for chatbot functionality, ensuring secure and efficient patient-doctor interactions.",
+        image:"Doctolib.png",
+        link1:'https://www.linkedin.com/in/pranesh-s-cse/',
+        link2:'https://doctolib-1-frontend.onrender.com/'
+    },
+    {
+        title:"Learn 2 Code",
+        des:"Learn to Code is an online learning platform that helps users develop web development skills in HTML, CSS, and JavaScript. It features an interactive coding environment, allowing users to practice coding online and download source code in PDF format. Built with Node.js, Express.js, and MySQL, the platform ensures smooth performance and efficient data management. Hosted on Onrender.com, it provides a hands-on learning experience for aspiring web developers.",
+        link1:'https://www.linkedin.com/in/pranesh-s-cse/',
+        link2:''
+    },
+]
+
+
+
+export const experience = [
+    {
+        company:'Cognifyz',
+        role:'FrontEnd Developer',
+        des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur illum nam, et quas placeat magnam, quaerat voluptatibus, autem nisi blanditiis temporibus reiciendis. Facere dignissimos quibusdam unde nisi blanditiis, amet eveniet!'
+    },
+    {
+        company:'Inpro Solution',
+        role:'FrontEnd Developer',
+        des:'I worked at Inpro Solution for six years, developing and managing projects using the MERN stack (MongoDB, Express.js, React.js, Node.js). My role involved designing scalable web applications, optimizing performance, and integrating advanced features. I collaborated with teams to build dynamic and user-friendly solutions, ensuring seamless functionality. This experience strengthened my expertise in full-stack development, database management.'
+    },
+]
